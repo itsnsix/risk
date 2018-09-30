a3cd4a3e96824284c9eeca0845c58b4ee36152e0
Lars Walter <larswalwes@gmail.com>
2018-09-30 23:52:33 +0200

    Move project images to resources.

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,8 @@ mix.js('resources/assets/js/app.js', 'public/js')
     .js('resources/assets/workers/labler.js', 'public/js/labler.js')
     .sass('resources/assets/sass/app.scss', 'public/css')
 
+    .copy('resources/assets/images/*.*', 'public/images')
+
     .js('node_modules/dragscroll/dragscroll.js', 'public/js/vendor.js')
 
     .version();
